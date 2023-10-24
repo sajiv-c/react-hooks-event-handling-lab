bae2b5e4d0a3ecf1c820d998efcef1487a7a8f17
Sajiv Chandrasekaran <sajivc88@gmail.com>
2023-10-23 21:58:59 -0400

    completed lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function EyesOnMe() {
+    function handleFocus(event) {
+        event.preventDefault();
+        console.log("Good!");
+    }
+
+    function handleBlur(event) {
+        event.preventDefault();
+        console.log("Hey! Eyes on me!");
+      }
+
+    return (
+        <div>
+            <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
